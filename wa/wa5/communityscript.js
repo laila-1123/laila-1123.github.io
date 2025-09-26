6e63b3e1d48ff0c9e17ec1a618a60904fd48b675
laila-1123 <laba5854@colorado.edu>
2025-09-26 12:40:07 -0600

    new books

--- wa/wa5/communityscript.js
+++ wa/wa5/communityscript.js
@@ -7,3 +7,35 @@ function toggleMenu (){
 }
 
 navToggle.addEventListener('click', toggleMenu);
+
+const filterButtons = document.querySelectorAll(".filter-buttons button");
+const books = document.querySelectorAll(".book");
+let activeFilter = null
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const filter = button.getAttribute("data-filter");
+        
+        if (activeFilter === filter) {
+            activeFilter = null;
+            button.classList.remove("active");
+            books.forEach(book => book.style.display = "");
+            return;
+        }
+        
+        activeFilter = filter;
+
+        filterButtons.forEach(btn => btn.classList.remove("active"));
+
+        button.classList.add("active");
+        
+        //filtering
+        books.forEach(book => {
+            if (filter === 'all' || book.getAttribute("data-genre") === filter) {
+        book.style.display = '';
+        } else {
+        book.style.display = 'none';
+        }
+    });
+});
+});
